e153d953e5d9dcc837ff2954d9e6da1ec2fd29b0
CodeBlastr <github@razorit.com>
2016-02-29 18:32:35 -0500

    moving back to username prefixed plugins

---
+++ config/routes.php
@@ -0,0 +1,28 @@
+<?php
+use Cake\Routing\Router;
+
+//Router::plugin(
+//    'Users',
+//    ['path' => '/users'],
+//    function ($routes) {
+//        $routes->fallbacks('DashedRoute');
+//    }
+//);
+
+Router::prefix('dashboard', function ($routes) {
+    $routes->plugin('Users', function ($routes) {
+        $routes->connect('/:controller');
+    });
+    $routes->extensions(['json', 'xml']);
+    Router::connect('/api/users/register', ['controller' => 'Users', 'action' => 'add', 'prefix' => 'dashboard']); // because crud (might remove)
+    $routes->fallbacks('DashedRoute');
+});
+
+Router::prefix('api', function ($routes) {
+    $routes->plugin('Users', function ($routes) {
+        $routes->connect('/:controller');
+    });
+    $routes->extensions(['json', 'xml']);
+    Router::connect('/api/users/register', ['controller' => 'Users', 'action' => 'add', 'prefix' => 'api']);
+    $routes->fallbacks('InflectedRoute');
+});
