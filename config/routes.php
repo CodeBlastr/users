e53809d1ade0b6482b1bf8c6714c269dbbb56c69
CodeBlastr <github@razorit.com>
2016-02-29 20:06:02 -0500

    pausing work on this

--- config/routes.php
+++ config/routes.php
@@ -9,20 +9,20 @@ use Cake\Routing\Router;
 //    }
 //);
 
-Router::prefix('dashboard', function ($routes) {
-    $routes->plugin('Users', function ($routes) {
-        $routes->connect('/:controller');
-    });
-    $routes->extensions(['json', 'xml']);
-    Router::connect('/api/users/register', ['controller' => 'Users', 'action' => 'add', 'prefix' => 'dashboard']); // because crud (might remove)
-    $routes->fallbacks('DashedRoute');
-});
-
-Router::prefix('api', function ($routes) {
-    $routes->plugin('Users', function ($routes) {
-        $routes->connect('/:controller');
-    });
-    $routes->extensions(['json', 'xml']);
-    Router::connect('/api/users/register', ['controller' => 'Users', 'action' => 'add', 'prefix' => 'api']);
-    $routes->fallbacks('InflectedRoute');
-});
+//Router::prefix('dashboard', function ($routes) {
+//    $routes->plugin('Users', function ($routes) {
+//        $routes->connect('/:controller');
+//    });
+//    $routes->extensions(['json', 'xml']);
+//    Router::connect('/api/users/register', ['controller' => 'Users', 'action' => 'add', 'prefix' => 'dashboard']); // because crud (might remove)
+//    $routes->fallbacks('DashedRoute');
+//});
+//
+//Router::prefix('api', function ($routes) {
+//    $routes->plugin('Users', function ($routes) {
+//        $routes->connect('/:controller');
+//    });
+//    $routes->extensions(['json', 'xml']);
+//    Router::connect('/api/users/register', ['controller' => 'Users', 'action' => 'add', 'prefix' => 'api']);
+//    $routes->fallbacks('InflectedRoute');
+//});
