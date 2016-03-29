be25e406cc8bc278c353a78c2791cd3b6a468b83
CodeBlastr <github@razorit.com>
2016-03-28 22:46:31 -0400

    added a virtual field

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -17,6 +17,7 @@ use Cake\ORM\Entity;
  */
 class User extends Entity
 {
+    protected $_virtual = ['full_name'];
 
     /**
      * Fields that can be mass assigned using newEntity() or patchEntity().
@@ -53,5 +54,11 @@ class User extends Entity
     protected function _setPassword($password)
     {
         return (new DefaultPasswordHasher)->hash($password);
+    }
+
+    protected function _getFullName()
+    {
+        return $this->_properties['first_name'] . '  ' .
+        $this->_properties['last_name'];
     }
 }
