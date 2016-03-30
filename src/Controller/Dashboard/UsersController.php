ee1485ebfac7c64e06e308e7c0480041c004f863
CodeBlastr <github@razorit.com>
2016-03-30 01:22:03 -0400

    let permissions manage this controller

--- src/Controller/Dashboard/UsersController.php
+++ src/Controller/Dashboard/UsersController.php
@@ -1,18 +1,26 @@
 <?php
-
 namespace CodeBlastr\Users\Controller\Dashboard;
 
 use App\Controller\AppController;
 use CodeBlastr\Users\Model\Table;
-use Cake\Event\Event;
-
-//temp / delete
-use Cake\Core\Plugin;
+use Cake\Core\Configure;
 
 class UsersController extends AppController
 {
     use \Crud\Controller\ControllerTrait;
 
+    /**
+     * Permissions management turned on
+     *
+     * @var bool
+     */
+    public $permissions = true;
+
+    /**
+     * Paginate settings
+     *
+     * @var array
+     */
     public $paginate = [
         'page' => 1,
         'limit' => 10,
@@ -39,5 +47,4 @@ class UsersController extends AppController
         ]);
         parent::initialize();
     }
-
 }
